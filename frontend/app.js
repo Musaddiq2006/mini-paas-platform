9f4cb30d09c28e10b4be5ec132bb1daaa26703b5
Musaddiq2006 <160424733468@mjcollege.ac.in>
2026-08-09 23:57:25 +0530

    feat: linked index.html to student and evaluator pages

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,27 @@
 function handleUpload(){
     const projectName = document.getElementById("projectName").value;
     const zipFile = document.getElementById("zipFile");
-    if(projectName==NaN){
+    if(projectName.trim()==""){
         alert("Please Enter a project Name!");
         return;
     }
-    if(fileInput.files.length ===0){
+    if(zipFile.files.length ===0){
         alert("Please upload a zip file!");
         return;
     }
-    const fileName = fileInput.files[0].name;
+    const fileName = zipFile.files[0].name;
     alert("Project: "+ projectName+ " with file: "+ fileName+ " ready for deployment");
 
+}
+function unlockEvaluator(){
+    const pinInput= document.getElementById("evaluatorPin").value;
+    const overlay = document.getElementById("modalOverlay");
+    if(pinInput==="1234"){
+        overlay.style.display="none";
+        alert("Permission Granted!");
+        
+    }
+    else{
+        alert("Incorrect PIN! Try Again")
+    }
 }
